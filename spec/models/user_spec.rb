39b072bb7b884afa39212c19f438dbb0b7c4df08
Travis Evans <travis909@gmail.com>
2019-11-11 18:49:38 -0500

    tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+
+  describe 'valid user' do
+    context 'email' do
+      it 'should contain an email address' do
+        user.email = nil
+        expect(user).not_to be_valid
+      end
+      it 'should have an email with @' do
+        user.email = 'testtest.com'
+        expect(user).not_to be_valid
+      end
+      it 'should have a unique email' do
+        (user1, user2 = FactoryBot.create(:user))
+        expect(user2).to eq(nil)
+      end
+    end
+    it 'should have a password' do
+      user.password_digest = nil
+      expect(user).not_to be_valid
+    end
+  end
 end
