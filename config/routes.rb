ea50778f12cfb62c076ac7a53007dace90c46bf6
Travis Evans <travis909@gmail.com>
2019-11-11 18:06:32 -0500

    Set  the versioning namespaces for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1, defaults: { format: :json } do
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
 
     end
   end
