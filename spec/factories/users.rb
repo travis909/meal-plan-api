f8aab3e92eb8bc9331b849dace19213f8ff7b071
Travis Evans <travis909@gmail.com>
2019-11-11 18:19:40 -0500

    created user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user do
-    email { "MyString" }
-    password_digest { "MyString" }
+    email { Faker::Internet.email }
+    password_digest { 'password' }
   end
 end
