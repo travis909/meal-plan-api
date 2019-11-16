af1232ddbf52b8180d88b0027763475e84a83560
Travis Evans <travis909@gmail.com>
2019-11-16 17:33:30 -0500

    Setup tokens controller

---
+++ app/controllers/api/v1/tokens_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    # app/controllers/api/v1/tokens_controller.rb
+    class TokensController < ApplicationController
+      def create
+        @user = User.find_by_email(user_params[:email])
+        if @user&.authenticate(user_params[:password])
+          render json: {
+            token: JsonWebToken.encode(user_id: @user.id),
+            email: @user.email
+          }
+        else
+          head :unauthorized
+        end
+      end
+
+      private
+
+      # Only allow a trusted parameter "white list" through.
+      def user_params
+        params.require(:user).permit(:email, :password)
+      end
+    end
+  end
+end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,46 +1,50 @@
 # frozen_string_literal: true
 
-# app/controllers/api/v1/users_controller.rb
-class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: %i[show update destroy]
+module Api
+  module V1
+    # app/controllers/api/v1/users_controller.rb
+    class UsersController < ApplicationController
+      before_action :set_user, only: %i[show update destroy]
 
-  # GET /users/1
-  def show
-    render json: User.find(params[:id])
-  end
+      # GET /users/1
+      def show
+        render json: User.find(params[:id])
+      end
 
-  # POST /users/1
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      render(json: { status: 'created' })
-    else
-      render(json: { errors: 'unprocessable_entry' })
+      # POST /users/1
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          render(json: { status: 'created' })
+        else
+          render(json: { errors: 'unprocessable_entry' })
+        end
+      end
+
+      # PATCH/PUT /users/1
+      def update
+        if @user.update(user_params)
+          render(json: { status: 'updated' })
+        else
+          render(json: { errors: 'unprocessable_entry' })
+        end
+      end
+
+      # DELETE /users/1
+      def destroy
+        @user.destroy
+        head 204
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:email, :password)
+      end
+
+      def set_user
+        @user = User.find(params[:id])
+      end
     end
-  end
-
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render(json: { status: 'updated' })
-    else
-      render(json: { errors: 'unprocessable_entry' })
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-    head 204
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :password)
-  end
-
-  def set_user
-    @user = User.find(params[:id])
   end
 end
